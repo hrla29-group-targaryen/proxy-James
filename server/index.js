7aff60c57c969707da546bde82d6ffe910236170
Jongho Kim <april9288@gmail.com>
2019-05-26 14:26:35 -0700

    hmmm

--- server/index.js
+++ server/index.js
@@ -10,12 +10,70 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-//my proxy server
-app.use('/intro/bundle.js',proxy({target:'http://localhost:3001/', pathRewrite:{'^/intro':'/'}}))
-app.use('/menu/bundle.js',proxy({target:'http://localhost:3002/', pathRewrite:{'^/menu':'/'}}))
+//
+//=================================================================>
+//
+
+// app.use('/api', proxy ({
+//  target:'http://localhost:3000',
+//  router: {
+//    '/intro/*': 'http://localhost:3001',
+//    '/menu/*': 'http://localhost:3002',
+//  },
+//  changeOrigin: true
+// }))
+// app.use(express.static(path.resolve(__dirname, "../static")))
+
+//
+//=================================================================>
+//
+
+
+
+let restaurantID
+
+app.use('/api/intro', proxy({
+	target:'http://localhost:3000/',
+	changeOrigin: true,
+	router: (req) => {
+    return `http://localhost:3001/api/intro/?id=${restaurantID}`;
+	}
+}))
+
+app.use('/api/menu', proxy({
+	target:'http://localhost:3000/',
+	changeOrigin: true,
+	router: (req) => {
+    return `http://localhost:3002/api/menu/?id=${restaurantID}`;
+	}
+}))
+
+const middleware = (req,res,next) => {
+	const { id } = req.query
+	console.log("middleware got a query : ", id)
+	restaurantID = id
+	next()
+}
 
 //serving static files
-app.use(express.static(path.resolve(__dirname, "../static")))
+app.get('/', middleware, express.static(path.resolve(__dirname, "../static")))
 
 //listening on port 3000
 app.listen(PORT, ()=> console.log("Server is up and running on", PORT))
+
+
+
+
+// my proxy server
+// app.use('/api/intro',proxy({target:'http://localhost:3001/'}))
+// app.use('/api/menu',proxy({target:'http://localhost:3002/'}))
+
+// app.use('/api', proxy ({
+//  target:'http://localhost:3000',
+//  router: {
+//    '/intro': 'http://localhost:3001',
+//    '/menu': 'http://localhost:3002',
+//  },
+//  changeOrigin: true
+// }))
+
